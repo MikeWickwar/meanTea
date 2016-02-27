01dcb75204bb79088a9ecbaad7e7657f0d6afdfb
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-27 07:57:08 -0700

    main partial installed

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ var app = angular.module('meantea', ['ngRoute']);
 app.config(function($routeProvider) {
     $routeProvider
       .when('/', {
-        controller: "MainController"
+        controller: "MainController",
+        templateUrl: "js/partials/main_shopping.html"
       })
       .otherwise({
         template: '<div><h1>No Page Located Here</h1></div>'
