0756f0f262a84b43392964cf27b12295920d5da6
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-27 14:51:09 -0700

    wired up shopping cart partial

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ app.config(function($routeProvider) {
         controller: "MainController",
         templateUrl: "js/partials/main_shopping.html"
       })
+      .when('/checkout', {
+        controller: "BagController",
+        templateUrl: "js/partials/checkout.html"
+      })
       .otherwise({
         template: '<div><h1>No Page Located Here</h1></div>'
       })
---
+++ js/controllers/bag_controller.js
@@ -0,0 +1,4 @@
+app.controller('BagController', ['$scope','$http','jsonService','catsService','cartService',
+ function ($scope, $http, jsonService, catsService, cartService) {
+  console.log('made it to bag control');
+}])
