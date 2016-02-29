067402bf6ce5d56d4007802f35298df5afefa337
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-28 17:30:31 -0700

    got working total showing on cart page!;

--- js/controllers/bag_controller.js
+++ js/controllers/bag_controller.js
@@ -9,5 +9,16 @@ app.controller('BagController', ['$scope','$http','jsonService','catsService','c
   };
   $scope.cart().then(function (cart) {
     $scope.bigcart = cart;
+    $scope.cartTotal = function () {
+      var total = 0;
+      cart.forEach(function (thingInCart) {
+        console.log(thingInCart.item.price);
+        total += parseInt(thingInCart.item.price) * parseInt(thingInCart.quantity);
+        console.log(total);
+        $scope.total =  total
+      })
+    }
+    var total = $scope.cartTotal();
+    console.log(total);
   })
 }])
