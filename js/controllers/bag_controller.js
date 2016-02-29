bacce934c74c2464637090355825c6d3921d15c3
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-29 01:35:29 -0700

    working remove button

--- js/controllers/bag_controller.js
+++ js/controllers/bag_controller.js
@@ -3,16 +3,28 @@ app.controller('BagController', ['$scope','$http','jsonService','catsService','c
   console.log('made it to bag control');
   $scope.total = 0000
   $scope.toggleQ = true;
-  $scope.quantity=1;
+  $scope.quantity=0;
 
   $scope.edit = function (item, quantity) {
     $scope.toggleQ = !$scope.toggleQ
-    console.log(item.i.item.name,"+_+_+_+_+_+_");
+    console.log(item,"+_+_+_+_+_+_");
     $scope.cart().then(function (cart) {
       cart.forEach(function (thingInCart) {
         if (thingInCart.item.name === item.i.item.name){
           thingInCart.quantity = item.quantity;
           $scope.cartTotal();
+        }
+      })
+    })
+  }
+  $scope.remove = function (item) {
+    console.log('hello', item);
+    $scope.cart().then(function (cart) {
+      cart.forEach(function (thingInCart) {
+        if (thingInCart.item.name === item.item.name) {
+          console.log(thingInCart);
+          $scope.total -= thingInCart.item.price * thingInCart.quantity
+          cart.splice(cart.indexOf(thingInCart), 1);
         }
       })
     })
